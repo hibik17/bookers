61216d9e13c54bad222f586b4469d186a9b2b355
hibik17 <hibiki@macbook>
2022-03-17 14:16:01 +0900

    follow周りの表示の設定完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
-  has_many :followers, through: :relationships, source: :follower
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   
   # active_storageを使った画像の設定
   has_one_attached :profile_image

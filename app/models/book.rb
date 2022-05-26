994834f8f527e3f43f45dd9fde7c2e41be382524
hibik17 <hibiki@macbook>
2022-05-26 16:54:33 +0900

    7a done

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,10 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites
-  
+
+  # set the relationship between book and favorites
+  has_many :favorited_users, through: :favorites, source: :user
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   

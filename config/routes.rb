4ad4da82c32079cc25070c4fac6d952db33543db
hibik17 <hibiki@macbook>
2022-05-27 15:55:11 +0900

    data base is ready

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,9 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/search', to: 'searches#search'
+
+  # DM
+  resources :users, only: [:show,:edit,:update]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
 end

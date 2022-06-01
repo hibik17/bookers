5b3954f615d77fd853494f2575ac2cad46275487
hibik17 <hibiki@macbook>
2022-06-01 11:14:18 +0900

    users/book create error revise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @user=User.find(params[:id])
-    @books = Book.new
+    @book = Book.new
     @books = @user.books
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)

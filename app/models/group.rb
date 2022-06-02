ad9862304f163059d4980ae981dc4ec813868879
hibik17 <hibiki@macbook>
2022-06-02 19:59:44 +0900

    7c complete

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,11 @@ class Group < ApplicationRecord
   validates :introduction, presence: true
 
   has_one_attached :group_image
+  def get_group_image(weight, height)
+    unless self.group_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      group_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    self.group_image.variant(resize_to_fill: [weight,height]).processed
+  end
 end

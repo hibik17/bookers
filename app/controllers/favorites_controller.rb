a23198051ee7d425512f2adbdf2bc012202f3c53
hibik17 <hbtatsu@gmail.com>
2022-06-12 00:04:01 +0900

    add guest user login

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,9 @@
 class FavoritesController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.new(book_id: @book.id)
+    binding.pry
+    # @favorite = current_user.favorites.new(book_id: @book.id)
+    @favorites = @book.favorites.users << current_user
     @favorite.save
     render 'replace_btn'
   end
